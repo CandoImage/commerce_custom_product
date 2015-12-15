d7fcfa16e96fe025cf5e8c85a438060d94d830ab
Peter Philipp <peter.philipp@cando-image.com>
2015-12-14 20:59:28 -0500

    Added views integration to provide customizable checkout review display.

--- commerce_custom_product.api.php
+++ commerce_custom_product.api.php
@@ -1,17 +1,15 @@
 <?php
-
 /**
  * @file
  * Documents hooks provided by the Customizable Products module.
  */
 
-
 /**
  * Allows modules to react to the creation of a new product line item type.
  *
- * @param $line_item_type
+ * @param array $line_item_type
  *   The product line item type info array.
- * @param $skip_rebuild
+ * @param bool $skip_rebuild
  *   Boolean indicating whether or not this save will result in the menu being
  *   rebuilt; if FALSE, your hook should not perform similar rebuild operations.
  *
@@ -24,9 +22,9 @@ function hook_commerce_custom_product_line_item_type_insert($line_item_type, $sk
 /**
  * Allows modules to react to the update of a product line item type.
  *
- * @param $line_item_type
+ * @param array $line_item_type
  *   The product line item type info array.
- * @param $skip_rebuild
+ * @param bool $skip_rebuild
  *   Boolean indicating whether or not this save will result in the menu being
  *   rebuilt; if FALSE, your hook should not perform similar rebuild operations.
  *
@@ -39,11 +37,11 @@ function hook_commerce_custom_product_line_item_type_update($line_item_type, $sk
 /**
  * Allows modules to react to the deletion of a product line item type.
  *
- * @param $line_item_type
+ * @param array $line_item_type
  *   The product line item type info array.
  *
  * @see commerce_custom_product_line_item_type_delete()
  */
 function hook_commerce_custom_product_line_item_type_delete($line_item_type) {
-  // No example
+  // No example.
 }
